36d231e52656373604f494d12d9b27a5efdf8305
Nati Forish <forish.nati@gmail.com>
2024-03-27 22:53:13 +0200

    Word: done.
    + added pdf instructions

--- RoadStone1/Word.java
+++ RoadStone1/Word.java
@@ -1,7 +1,68 @@
 package test;
 
+import java.util.Arrays;
 
-public class Word {
+public class Word extends Tile {
+    final private Tile[] wordTiles;
+    final private int row, col;
+    final private boolean vertical;
 
+    public Word(Tile[] wordTiles, int row, int col, boolean vertical) {
+        this.wordTiles = wordTiles;
+        this.row = row;
+        this.col = col;
+        this.vertical = vertical;
+    }
+
+    public Tile[] getWordTiles() {
+        return wordTiles;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(wordTiles);
+        result = prime * result + row;
+        result = prime * result + col;
+        result = prime * result + (vertical ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Word other = (Word) obj;
+        if (!Arrays.equals(wordTiles, other.wordTiles))
+            return false;
+        if (row != other.row)
+            return false;
+        if (col != other.col)
+            return false;
+        if (vertical != other.vertical)
+            return false;
+        return true;
+    }
+
+    
+
+    
 	
 }
