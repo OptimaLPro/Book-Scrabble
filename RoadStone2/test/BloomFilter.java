e8249fdb981e75c1db51be352affd6f840270be0
Nati Forish <forish.nati@gmail.com>
2024-04-16 11:42:42 +0300

    Finish RoadStone2 [100]

--- RoadStone2/test/BloomFilter.java
+++ RoadStone2/test/BloomFilter.java
@@ -3,49 +3,40 @@ package test;
 import java.util.BitSet;
 import java.security.MessageDigest;
 import java.math.BigInteger;
-;
 
 public class BloomFilter {
     private BitSet bitSet;
     private int size;
-    // private int hashNum;
     private MessageDigest[] algs;
-    private BigInteger[] hashValues;
 
     public BloomFilter(int size, String... algs) {
         this.size = size;
-        this.bitSet = new BitSet(size);
-        this.algs = new MessageDigest[algs.length];
-        this.hashValues = new BigInteger[algs.length];
+        this.bitSet = new BitSet();
         try {
+            this.algs = new MessageDigest[algs.length];
             for (int i = 0; i < algs.length; i++) {
                 this.algs[i] = MessageDigest.getInstance(algs[i]);
             }
         } catch (Exception e) {
             e.printStackTrace();
+            throw new RuntimeException("Failed to initialize BloomFilter");
         }
     }
 
     public void add(String word) {
-        for (int i = 0; i < algs.length; i++) {
-            algs[i].update(word.getBytes());
-            byte[] digest = algs[i].digest();
-            hashValues[i] = new BigInteger(1, digest);
-            int hash = hashValues[i].intValue();
-            hash = Math.abs(hash);
-            hash = hash % size;
+        for (MessageDigest alg : algs) {
+            byte[] digest = alg.digest(word.getBytes());
+            BigInteger hashValue = new BigInteger(1, digest);
+            int hash = Math.abs(hashValue.intValue()) % size;
             bitSet.set(hash);
         }
     }
 
     public boolean contains(String word) {
-        for (int i = 0; i < algs.length; i++) {
-            algs[i].update(word.getBytes());
-            byte[] digest = algs[i].digest();
-            hashValues[i] = new BigInteger(1, digest);
-            int hash = hashValues[i].intValue();
-            hash = Math.abs(hash);
-            hash = hash % size;
+        for (MessageDigest alg : algs) {
+            byte[] digest = alg.digest(word.getBytes());
+            BigInteger hashValue = new BigInteger(1, digest);
+            int hash = Math.abs(hashValue.intValue()) % size;
             if (!bitSet.get(hash)) {
                 return false;
             }
@@ -53,11 +44,13 @@ public class BloomFilter {
         return true;
     }
 
-
     @Override
     public String toString() {
-        return bitSet.toString();
+        String s = "";
+        for (int i = 0; i < bitSet.length(); i++) {
+            s += bitSet.get(i) ? "1" : "0";
+        }
+        return s;
     }
-
 
 }
